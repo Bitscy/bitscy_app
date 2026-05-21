fb1df3f6382ec271be07c2cc179908c3ca3246cc
Sandijigs <idjigheresandra@gmail.com>
2026-05-21 16:54:37 +0100

    chore: pin dependencies, add directUrl to prisma, update next-pwa config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    skipWaiting: true,
+  },
 });
 
 const nextConfig = {
